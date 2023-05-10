5a5be9f41ec22bcaa4bfde9b443b67f80abda1f1
Sakralen <grigoriy.e.spasov@gmail.com>
2023-05-10 04:05:54 +0300

    homework 2 is done

--- src/test/java/edu/hsai/homework2/DriverSetup.java
+++ src/test/java/edu/hsai/homework2/DriverSetup.java
@@ -1,5 +1,35 @@
 package edu.hsai.homework2;
 
-public class DriverSetup {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
+public class DriverSetup {
+    protected static WebDriver driver;
+
+    @BeforeTest
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // 1. Open test site by URL
+        driver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
+
+        // 3. Perform login
+        driver.findElements(By.className("dropdown-toggle")).get(1).click();
+        driver.findElement(By.id("name")).sendKeys("Roman");
+        driver.findElement(By.id("password")).sendKeys("Jdi1234");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @AfterTest
+    public static void exit() {
+        //10. Close Browser
+        driver.close();
+    }
 }
--- src/test/java/edu/hsai/homework2/Task2Test.java
+++ src/test/java/edu/hsai/homework2/Task2Test.java
@@ -1,5 +1,53 @@
 package edu.hsai.homework2;
 
-public class Task2Test extends DriverSetup {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class Task2Test extends DriverSetup {
+    @Test
+    public void testBrowserTitle() {
+        // 2. Assert browser title
+        assertEquals(driver.getTitle(), "Home Page");
+    }
+
+    @Test
+    public void testLogin() {
+        // 4. Assert Username is loggined
+        assertEquals(driver.findElement(By.id("user-name")).getText(), "ROMAN IOVLEV");
+    }
+
+    @Test
+    public void testElements() {
+        // 5. Open through the header menu Service -> Different Elements Page
+        driver.findElements(By.className("dropdown-toggle")).get(0).click();
+        driver.findElement(By.xpath("/html/body/header/div/nav/ul[1]/li[3]/ul/li[8]/a")).click();
+
+        // 6. Select checkboxes
+        driver.findElements(By.className("label-checkbox")).stream()
+                .filter(checkbox -> checkbox.getText().equals("Water") || checkbox.getText().equals("Wind"))
+                .forEach(WebElement::click);
+
+        // 7. Select radio
+        driver.findElements(By.className("label-radio")).stream()
+                .filter(radio -> radio.getText().equals("Selen"))
+                .forEach(WebElement::click);
+
+        // 8. Select in dropdown
+        driver.findElements(By.tagName("option")).stream()
+                .filter(option -> option.getText().equals("Yellow"))
+                .forEach(WebElement::click);
+
+        // 9. Assert logs
+        var logLines = Arrays.stream(driver.findElement(By.cssSelector("ul.panel-body-list.logs")).getText()
+                .split("\n")).map(line -> line.substring(9)).toList();
+
+        assertEquals(logLines, List.of("Colors: value changed to Yellow", "metal: value changed to Selen",
+                "Wind: condition changed to true", "Water: condition changed to true"));
+    }
 }
